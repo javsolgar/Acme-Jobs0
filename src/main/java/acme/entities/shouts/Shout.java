75687b51a369bcb9b8624edaf492622486d44347
Javi <javsolgar@alum.us.es>
2019-10-15 16:38:48 +0200

    Funcional lab practise

--- src/main/java/acme/entities/shouts/Shout.java
+++ src/main/java/acme/entities/shouts/Shout.java
@@ -16,7 +16,6 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-
 public class Shout extends DomainEntity {
 
 	// Serialisation identifier --------------------------------------------------------------------------------
